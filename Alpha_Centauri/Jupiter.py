fb58faa650be3222018a533bf647c4cf70959dc9
francapolack <frmuuoo@gmail.com>
2026-08-07 08:05:48 -0300

    cambio en la funcion display2opciones + integrando opciones

--- Alpha_Centauri/Jupiter.py
+++ Alpha_Centauri/Jupiter.py
@@ -51,12 +51,12 @@ def exploracion():
         display_texto(window,600,600,"MARK:Ve a ayudarlo!")
 
         cambio_texto(window,fondo,fondo_rect,jugador.textura,jugador.hitbox)
-        display_2_opciones(window,600,700,600,"Ayudar al alien","Escapar")
+        ayudar=display_opciones(window,600,300,500,"AYUDAR AL ALIEN",50)
+        escapar=display_opciones(window,600,300,500,"ESCAPAR",50)
+        window.blit(alien_1.textura,alien_1.hitbox)
 
-      if jugador.hitbox.colliderect(alien_1.hitbox):
-              voz_gigantes.play()
-              display_texto(window,600,600,"...")#TODO: poner 600 600 fijo en la def display texto
-      
+        if pygame.mouse.get_pressed()[0] and ayudar.hitbox.collidepoint(mouse_pos):
+            print("meow")      
 
 
 def main():
--- Alpha_Centauri/class_definitions.py
+++ Alpha_Centauri/class_definitions.py
@@ -25,7 +25,7 @@ def movimiento(jugador):
             jugador.hitbox.move_ip(0,-jugador.VELOCIDAD)
     elif tecla[pygame.K_DOWN] or tecla[pygame.K_s]:
             jugador.hitbox.move_ip(0,jugador.VELOCIDAD)
-    pass
+
 
 class NPC:
     def __init__(self,textura,x,y,escalax,escalay):
@@ -53,28 +53,18 @@ def display_texto(pan,ancho,alto,texto):
     txto=fuente.render(texto,True,BLANCO)
     pan.blit(txto,(caja_rect.x+100,caja_rect.y+45))
 
-def display_2_opciones(pan,ancho,alto1,alto2,texto1,texto2):
-    fuente=pygame.font.Font(None,36)
+def display_opciones(pan,ancho,alto,texto,distanciax):
+    fuente=pygame.font.SysFont("Consolas",36)
      #cajita 1
     ancho_caja=int(ancho)
-    alto_caja=int(alto1*0.15)
+    alto_caja=int(alto*0.40)
     caja_rect_1=pygame.Rect(0,0,ancho_caja,alto_caja)
-    caja_rect_1.center=(ancho+160,alto1)
+    caja_rect_1.center=(ancho+160,alto)
     pygame.draw.rect(pan, AZUL_MARINO, caja_rect_1, border_radius=8)
     pygame.draw.rect(pan, TURQUESA, caja_rect_1, width=3, border_radius=8)
     #texto cajita1
-    txto1=fuente.render(texto1,True,BLANCO)
-    pan.blit(txto1,(caja_rect_1.x+100,caja_rect_1.y+45))
-    #cajita2
-    ancho_caja=int(ancho)
-    alto_caja=int(alto2*0.15)
-    caja_rect_2=pygame.Rect(0,0,ancho_caja,alto_caja)
-    caja_rect_2.center=(ancho+160,alto2)
-    pygame.draw.rect(pan, AZUL_MARINO, caja_rect_2, border_radius=8)
-    pygame.draw.rect(pan, TURQUESA, caja_rect_2, width=3, border_radius=8)
-    #texto cajita2
-    txto2=fuente.render(texto2,True,BLANCO)
-    pan.blit(txto2,(caja_rect_2.x//2,caja_rect_2.y//1))
+    txto1=fuente.render(texto,True,BLANCO)
+    pan.blit(txto1,(caja_rect_1.x+distanciax,caja_rect_1.y+40))
 
 
 def cambio_texto(pan,f,frect,j,jrect):
