580cf126234ed53ee552f044652328310031a868
gabilyn11 <malddonadoeve@gmail.com>
2026-08-28 02:49:13 -0300

    Add files via upload
    abierto a modificaciones

---
+++ Alpha_Centauri/config.py
@@ -0,0 +1,12 @@
+# config.py
+
+ANCHO_PANTALLA = 1000
+ALTO_PANTALLA = 600
+FPS = 60
+
+ALTO_MAPA_ASTRO = 58  
+FACTOR_ESCALA_MAPA = 1.6
+
+VELOCIDAD_JUGADOR = 4
+# Este archivo es solo para guardar las variables generales del juego 
+# y que el resto de los archivos estén más limpios.
